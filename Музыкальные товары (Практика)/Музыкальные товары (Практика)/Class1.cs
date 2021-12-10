e6bdb4ce2fead8ef840804ce7e438c50842c8d81
EgorFead <95931082+EgorFead@users.noreply.github.com>
2021-12-10 20:28:27 +0300

    Add files via upload

---
+++ Музыкальные товары (Практика)/Музыкальные товары (Практика)/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Музыкальные_товары__Практика_
+{
+    class Class1
+    {
+    }
+}
